73485cd461cd438692bb86683ee42add72ab3e25
Alex Phung <alex.phung124@gmail.com>
2024-01-31 20:13:06 -0500

    Complétion de la classe BaseShape

--- src/main/java/Shape/BaseShape.java
+++ src/main/java/Shape/BaseShape.java
@@ -9,149 +9,184 @@ import java.util.stream.Collectors;
 public class BaseShape extends Transform implements Cloneable {
     private final Collection<Point2d> coords;
 
-//helper function to clone a list of points 
-  public Collection<Point2d> cloneCoords(Collection<Point2d> coords) {
+    //helper function to clone a list of points
+    public Collection<Point2d> cloneCoords(Collection<Point2d> coords) {
         return coords.stream().map(Point2d::clone).collect(Collectors.toList());
     }
 
-    /** TODO
+    /**
      * Create a BaseShape with empty coordinades
      */
     public BaseShape() {
-        this.coords = null;
+        this.coords = new ArrayList<>();
     }
 
-    /** TODO
+    /**
      * Create a BaseShape from a collection of 2D points
+     *
      * @param coords The collection of 2D points
      */
     public BaseShape(Collection<Point2d> coords) {
-        this();
+        this.coords = cloneCoords(coords);
     }
 
-    /** TODO
+    /**
      * Add a deep copy of the 2D point to the Shape
+     *
      * @param coord 2D point to add
      * @return Updated BaseShape
      */
     public BaseShape add(Point2d coord) {
-        return null;
+        this.coords.add(coord.clone());
+        return this;
     }
 
-    /** TODO
+    /**
      * Add a deep copy of the 2D points of the shape to the current shape
+     *
      * @param shape Shape to add to the current shape
      * @return Updated BaseShape
      */
     public BaseShape add(BaseShape shape) {
-        return null;
+        for (Point2d p2d : shape.coords) {
+            this.add(p2d);
+        }
+        return this;
     }
 
-    /** TODO
+    /**
      * Add a deep copy of the points in the collection to the shape
+     *
      * @param coords Collections of point to add
      * @return Updated BaseShape
      */
     public BaseShape addAll(Collection<Point2d> coords) {
-        return null;
+        for (Point2d p2d : coords) {
+            this.add(p2d);
+        }
+        return this;
     }
 
-    /** TODO
+    /**
      * Remove the 2D point from the shape
+     *
      * @param coord Point to remove
      * @return Updated BaseShape
      */
     public BaseShape remove(Point2d coord) {
-        return null;
+        this.coords.remove(coord);
+        return this;
     }
 
-    /** TODO
+    /**
      * Remove the 2D points in the shape from the current shape
+     *
      * @param shape Shape containing the points to remove
      * @return Updated BaseShape
      */
     public BaseShape remove(BaseShape shape) {
-        return null;
+        for (Point2d p2d : shape.coords) {
+            this.remove(p2d);
+        }
+        return this;
     }
 
-    /** TODO
+    /**
      * Remove the 2D points in the collection from the current shape
+     *
      * @param coords Collection of 2D points to remove
      * @return Updated BaseShape
      */
     public BaseShape removeAll(Collection<Point2d> coords) {
-        return null;
+        for (Point2d p2d : coords) {
+            this.remove(p2d);
+        }
+        return this;
     }
 
-    /** TODO
+    /**
      *  Replace all the coords in a shape with new ones
+     *
      * @param newCoords new coords to replace the old one
      * @return Updated BaseShape
-     * */
-    public BaseShape replaceAll(Collection<Point2d> newCoords) { return null; }
+     */
+    public BaseShape replaceAll(Collection<Point2d> newCoords) {
+        this.removeAll(this.coords);
+        this.addAll(newCoords);
+        return this;
+    }
 
-    /** TODO
+    /**
      * Return a shallow copy of the coordinates of the shape
+     *
      * @return Shallow copy of all coordinates contained by this BaseShape
      */
     public Collection<Point2d> getCoords() {
-        return null;
+        return new ArrayList<>(coords);
     }
 
-    /** TODO
+    /**
      * Create and return a deep copy of the coordinates of the shape
+     *
      * @return Deep copy of all coordinates contained by this BaseShape
      */
     public Collection<Point2d> cloneCoords() {
-        return null;
+        return this.cloneCoords(this.coords);
     }
 
-    /** TODO
+    /**
+     *
      * @return Maximum X coordinate of the shape
      */
     public Double getMaxX() {
-        return null;
+        return !coords.isEmpty() ? this.coords.stream().max(Comparator.comparing(Point2d::X)).get().X() : -Double.MAX_VALUE;
     }
 
-    /** TODO
+    /**
+     *
      * @return Maximum Y coordinate of the shape
      */
     public Double getMaxY() {
-        return null;
+        return !coords.isEmpty() ? this.coords.stream().max(Comparator.comparing(Point2d::Y)).get().Y() : -Double.MAX_VALUE;
     }
 
-    /** TODO
+    /**
+     *
      * @return 2D Point containing the maximum X and Y coordinates of the shape
      */
     public Point2d getMaxCoord() {
-        return null;
+        return new Point2d(getMaxX(), getMaxY());
     }
 
-    /** TODO
+    /**
+     *
      * @return Minimum X coordinate of the shape
      */
     public Double getMinX() {
-        return null;
+        return !coords.isEmpty() ? this.coords.stream().min(Comparator.comparing(Point2d::X)).get().X() : Double.MAX_VALUE;
     }
 
-    /** TODO
+    /**
+     *
      * @return Minimum Y coordinate of the shape
      */
     public Double getMinY() {
-        return null;
+        return !coords.isEmpty() ? this.coords.stream().min(Comparator.comparing(Point2d::Y)).get().Y() : Double.MAX_VALUE;
     }
 
-    /** TODO
+    /**
+     *
      * @return 2D point containing the minimum X and Y coordinate of the shape
      */
     public Point2d getMinCoord() {
-        return null;
+        return new Point2d(getMinX(), getMinY());
     }
 
-    /** TODO
+    /**
+     *
      * @return Deep copy of the current shape
      */
     public BaseShape clone() {
-        return null;
+        return new BaseShape(cloneCoords());
     }
 }
